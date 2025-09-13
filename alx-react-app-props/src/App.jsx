e99f9c905e8f99dc8f7efeb4e9125a6f564e82a5
Emmanuel Ahenakwah <149573921+pee-21@users.noreply.github.com>
2025-09-13 16:51:51 +0000

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,24 +1,18 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './components/UserProfile';
-
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
-return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+export default App;
+
   );
 
    return (
