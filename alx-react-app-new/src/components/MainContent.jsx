d429bc21970fd2b3b99a46606a346591aee403a0
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-13 13:09:42 +0000

    a

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
