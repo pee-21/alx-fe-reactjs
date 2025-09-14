904c7d267894aca5a298d6d215ecd87b2ee599fe
Emmanuel Ahenakwah <149573921+pee-21@users.noreply.github.com>
2025-09-14 20:47:04 +0000

    Create SearchBar.jsx

---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { useRecipeStore } from '../recipeStore';
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+      style={{ margin: '10px', padding: '8px', width: '60%' }}
+    />
+  );
+};
+
+export default SearchBar;
