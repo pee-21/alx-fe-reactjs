2c6740546f3b2f77dcf93a4d376dcc5a68886cda
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-21 10:07:25 +0000

    a

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,3 +10,17 @@ export const fetchUserData = async (username) => {
     throw new Error("Looks like we can't find the user");
   }
 };
+
+export const fetchAdvancedUsers = async (username, location, repos) => {
+  let query = "";
+
+  if (username) query += `${username}+`;
+  if (location) query += `location:${location}+`;
+  if (repos) query += `repos:>=${repos}`;
+
+  const response = await axios.get(
+    `https://api.github.com/search/users?q=${query}`
+  );
+
+  return response.data.items;
+};
