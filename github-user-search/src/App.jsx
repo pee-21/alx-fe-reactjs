3e6d0cf0587a4db66162de30ecb99f7da2fca772
Emmanuel Ahenakwah <149573921+pee-21@users.noreply.github.com>
2025-09-21 16:21:46 +0000

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,39 +1,13 @@
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UserCard from "./components/UserCard";
-import { fetchGitHubUser } from "./services/githubApi";
+import React from "react";
 import Search from "./components/Search";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const handleSearch = async (username) => {
-    const data = await fetchGitHubUser(username);
-    setUser(data);
-  };
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>GitHub User Search</h1>
-      <SearchBar onSearch={handleSearch} />
-      <UserCard user={user} />
+    <div className="App bg-gray-100 min-h-screen p-4">
+      <h1 className="text-3xl font-bold text-center mb-6">GitHub Advanced User Search</h1>
+      <Search />
     </div>
   );
 }
- return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>GitHub User Search</h1>
-      <Search />
-    </div>
-  );
-
-   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <Search />
-    </div>
-  );
-
-
 
 export default App;
-
