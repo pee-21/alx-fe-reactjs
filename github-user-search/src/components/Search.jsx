e16d40d4cee4e8385367a4f89cd4b1d45f5f68b1
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-21 09:32:29 +0000

    s

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -54,4 +54,15 @@ const Search = () => {
   );
 };
 
+{user && (
+  <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+    <img src={user.avatar_url} alt={user.login} width={80} />
+    <h2>{user.name || user.login}</h2>
+    <p>@{user.login}</p>
+    <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+      View GitHub Profile
+    </a>
+  </div>
+)}
+
 export default Search;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com/users";
 
-// Function to fetch GitHub user data
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}/${username}`);
     return response.data;
   } catch (error) {
-    throw new Error("User not found");
+    throw new Error("Looks like we can't find the user");
   }
 };
