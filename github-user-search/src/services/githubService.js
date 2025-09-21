c878a898885f32591ed100d6600b1e973df1d3bc
Richmondfreeheart <kuamerichmond@gmail.com>
2025-09-21 09:52:15 +0000

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/users";
 
-export const fetchAdvancedUserSearch = async (username, location, minRepos) => {
-  let query = "";
-
-  if (username) query += `${username} in:login `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>=${minRepos} `;
-
+export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`${BASE_URL}?q=${encodeURIComponent(query)}`);
+    const response = await axios.get(`${BASE_URL}/${username}`);
     return response.data;
   } catch (error) {
-    throw new Error("Error fetching users");
+    throw new Error("Looks like we can't find the user");
   }
 };
