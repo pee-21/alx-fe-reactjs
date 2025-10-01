85e409dc350c4d72d234a0e751868cd66b62d796
Emmanuel Ahenakwah <149573921+pee-21@users.noreply.github.com>
2025-10-01 10:14:27 +0000

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,9 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // ✅ Add this
-  darkMode: false,
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
